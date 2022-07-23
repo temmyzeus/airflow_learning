c672ac1a7161e8ec7951c9d1be697905f6bc4b61
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-07-23 15:29:48 +0100

    Working Dag 1

--- dags/data_ingestion_dag/pipeline.py
+++ dags/data_ingestion_dag/pipeline.py
@@ -1,31 +1,62 @@
 import os
+import sqlite3
 from datetime import datetime, timedelta
+from textwrap import dedent
 from typing import Any
 
+import pandas as pd
 from airflow import DAG
+from airflow.models.variable import Variable
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-import pandas as pd
+
 
 home_dir = os.path.expanduser("~")
-raw_data_dir = os.path.join(home_dir, "raw_data")
+raw_data_dir = os.path.join(home_dir, "data", "raw_data")
+staging_data_dir = os.path.join(home_dir, "data", "staging")
 
-def extract_data():
-    bookings_df = pd.read_csv(os.path.join(raw_data_dir, "booking.csv"), low_memory=False)
+def transform_data():
+    bookings_df = pd.read_csv(
+        os.path.join(raw_data_dir, "booking.csv"), low_memory=False
+    )
     client_df = pd.read_csv(os.path.join(raw_data_dir, "client.csv"), low_memory=False)
     hotel_df = pd.read_csv(os.path.join(raw_data_dir, "hotel.csv"), low_memory=False)
 
-    print("Booking Data Shape:", bookings_df.shape)
-    print("Client Data Shape:", client_df.shape)
-    print("Hotel Data Shape:", hotel_df.shape)
+    bookings_df.booking_date = pd.to_datetime(
+        bookings_df.booking_date, infer_datetime_format=True
+    )
+    bookings_df.currency = "GBP"
 
-    return bookings_df, client_df, hotel_df
+    client_df = client_df.rename(
+        columns={"age": "client_age", "name": "client_name", "type": "client_type"}
+    )
+    hotel_df = hotel_df.drop(columns=["address"])
+    hotel_df = hotel_df.rename(
+        columns={"name": "hotel_name", "address": "hotel_address"}
+    )
 
-def extract_data1():
-    print("Hello World")
+    data = pd.merge(
+        left=bookings_df, right=client_df, on="client_id", how="inner"
+    ).merge(hotel_df, how="inner", on="hotel_id")
 
-def extract_data2():
-    print("Hello World")
+    data.to_csv(os.path.join(staging_data_dir, "hotel_bookings.csv"), index=False)
+
+
+def load_data():
+    table_name:str = "hotel_bookings"
+    conn = sqlite3.connect("/home/airflow/databases/hotel_bookings.db")
+    cursor = conn.cursor()
+
+    data = pd.read_csv(os.path.join(staging_data_dir, "hotel_bookings.csv"), low_memory=False)
+
+    schema = pd.io.sql.get_schema(data, name=table_name, con=conn)
+    schema = schema.replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS")
+    cursor.execute(schema)
+
+    data.to_sql(name=table_name, con=conn, if_exists="replace", index=False)
+    cursor.close()
+    conn.close()
+
 
 # with DAG()  as ingestion_dag:
 
@@ -36,21 +67,17 @@ ingestion_dag = DAG(
     description="Hotel bookings data pipeline",
     default_args=default_args,
     start_date=days_ago(2),
-    schedule_interval="@daily",
+    schedule_interval="@once",
     catchup=False,
     tags=["Ingest data from source"],
 )
 
-extract_data = PythonOperator(
-    task_id="Extract-Data", python_callable=extract_data, dag=ingestion_dag
-)
-
 transform_data = PythonOperator(
-    task_id="Transform-Data", python_callable=extract_data1, dag=ingestion_dag
+    task_id="Transform-Data", python_callable=transform_data, dag=ingestion_dag
 )
 
 load_data = PythonOperator(
-    task_id="Load-Data", python_callable=extract_data2, dag=ingestion_dag
+    task_id="Load-Data", python_callable=load_data, dag=ingestion_dag
 )
 
-extract_data >> transform_data >> load_data
+transform_data >> load_data
